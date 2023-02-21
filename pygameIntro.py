c4c0dcb8e51ba422f1d6a7e5a6837f600a0549cc
ZoutKoning <jfscyber9@gmail.com>
2023-02-21 16:47:46 -0500

    Co-authored-by: jreich02 <jreich02@users.noreply.github.com>

--- pygameIntro.py
+++ pygameIntro.py
@@ -4,8 +4,26 @@ SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 
 # Initialize game
 pygame.init()
-pygame.display.set_caption("hi 4160/6160")
+#window name
+pygame.display.set_caption("Pong-Game")
 surface = pygame.display.set_mode(SCREEN_SIZE)
+
+#Text Settings
+# color
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+# font
+font = pygame.font.Font('Freesansbold.ttf', 32)
+
+# text
+text = font.render('Game Over', True, green, blue)
+
+# Creates rect obj for the text surface obj
+textRect = text.get_rect()
+
+textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
 
 # Color of my window
 screenColor = (200, 200, 200)
@@ -18,9 +36,16 @@ pongSpeed = 1
 
 gamePong = pygame.Rect(rectX, rectY, pongWidth, pongHeight)
 
+# ceiling object - rectangle vars
+ceilingColor = (0, 0, 255)
+ceilingSize = ceilingWidth, ceilingHeight = SCREEN_WIDTH, 25
+ceilingPos = ceilingX, ceilingY =  0, 0
+
+gameCeiling = pygame.Rect(ceilingX, ceilingY, ceilingWidth, ceilingHeight)
+
 # player - rectangle vars
 rectColor = (255, 0, 0)
-rectSize = rectWidth, rectHeight = 100, 25
+rectSize = rectWidth, rectHeight = 100, 10
 rectPos = rectX, rectY = 200, 500
 rectSpeed = 2
 
@@ -41,6 +66,7 @@ def move_rect(gameRect):
 #move pong 
 def move_pong(gamePong):
     gamePong.move_ip(0, pongSpeed)
+    
 
 # Game loop
 while True:
@@ -53,13 +79,35 @@ while True:
     # Allow pong to move
     move_pong(gamePong)
     
-
+    #Collision - if pong collides with player rect
+    collidePlayer = pygame.Rect.colliderect(gameRect, gamePong)
+    if collidePlayer:
+        pongSpeed *=-1
+        
+    #Collision - if pong collides with display ceiling
+    collideCeiling = pygame.Rect.colliderect(gameCeiling, gamePong)
+    if collideCeiling:
+        pongSpeed *= -1
+    
+    # Text Display 
+    surface.blit(text, textRect)
+    #restrict - rect to display
     gameRect.clamp_ip(surface.get_rect())
+    
+    #restrict - pong to display
+    gamePong.clamp_ip(surface.get_rect())
+    
     #draw stuff to screen
     surface.fill(screenColor)
+    
     #draw - player
     pygame.draw.rect(surface, rectColor, gameRect)
+    
     #draw - pong
     pygame.draw.rect(surface, pongColor, gamePong)
-    #draw - display
+    
+    #draw - ceiling
+    pygame.draw.rect(surface, ceilingColor, gameCeiling)
+    
+    #draw - display (keep drawing till end)
     pygame.display.update()
