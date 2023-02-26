a73c7be1ad2b3237ddc169745fe721f2524e79e3
jreich02 <112362749+jreich02@users.noreply.github.com>
2023-02-26 10:43:29 -0500

    Add files via upload

--- pygameIntro.py
+++ pygameIntro.py
@@ -8,22 +8,6 @@ pygame.init()
 pygame.display.set_caption("Pong-Game")
 surface = pygame.display.set_mode(SCREEN_SIZE)
 
-#Text Settings
-# color
-green = (0, 255, 0)
-blue = (0, 0, 255)
-
-# font
-font = pygame.font.Font('Freesansbold.ttf', 32)
-
-# text
-text = font.render('Game Over', True, green, blue)
-
-# Creates rect obj for the text surface obj
-textRect = text.get_rect()
-
-textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
 
 # Color of my window
 screenColor = (200, 200, 200)
@@ -31,10 +15,10 @@ screenColor = (200, 200, 200)
 # pong - rectangle vars
 pongColor = (255, 255, 255)
 pongSize = pongWidth, pongHeight = 25, 25
-pongPos = rectX, rectY = 100, 100
-pongSpeed = 1
+pongPos = pongX, pongY = 100, 100
+pongSpeed = pongXspeed, pongYspeed = 0.2, 0.5
 
-gamePong = pygame.Rect(rectX, rectY, pongWidth, pongHeight)
+gamePong = pygame.Rect(pongX, pongY, pongWidth, pongHeight)
 
 # ceiling object - rectangle vars
 ceilingColor = (0, 0, 255)
@@ -64,8 +48,8 @@ def move_rect(gameRect):
         #gameRect.move_ip(0,rectSpeed)
 
 #move pong 
-def move_pong(gamePong):
-    gamePong.move_ip(0, pongSpeed)
+def move_pong(gamePong, pongX, pongY):
+    gamePong.update(pongX, pongY, pongWidth, pongHeight)
     
 
 # Game loop
@@ -77,20 +61,20 @@ while True:
     # Allow player to move
     move_rect(gameRect)
     # Allow pong to move
-    move_pong(gamePong)
+    pongX+=pongXspeed
+    pongY+=pongYspeed
+    move_pong(gamePong, pongX, pongY)
     
     #Collision - if pong collides with player rect
     collidePlayer = pygame.Rect.colliderect(gameRect, gamePong)
     if collidePlayer:
-        pongSpeed *=-1
+        pongYspeed *= -1
         
     #Collision - if pong collides with display ceiling
     collideCeiling = pygame.Rect.colliderect(gameCeiling, gamePong)
     if collideCeiling:
-        pongSpeed *= -1
+        pongYspeed*=-1
     
-    # Text Display 
-    surface.blit(text, textRect)
     #restrict - rect to display
     gameRect.clamp_ip(surface.get_rect())
     
